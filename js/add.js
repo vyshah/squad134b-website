5c5a5cb9827163ae61efd1ca9bfff2a9688a6f90
Scott Tang <mashup@Scotts-MacBook-Pro.local>
2015-11-12 23:04:25 -0800

    can now add habit to parse database

--- js/add.js
+++ js/add.js
@@ -1,3 +1,5 @@
+Parse.initialize("BoImLGZodfLfRb4hR2YkejAdYjtSnLriHunArwnP", "1oCKt23WJYH6jHrjtEqA8wCNRmva9Pfxuj32GLR8");
+
 $(document).ready(function () {
     function init() {
         if (localStorage["title"]) {
@@ -36,6 +38,69 @@ $('.stored').keyup(function() {
 });
 
 $('#save').click(function() {
-  window.localStorage.clear();
-  $('#localStorage').get(0).reset();
+    window.localStorage.clear();
+    $('#localStorage').get(0).reset();
 });
+
+
+function selectImage(name) {
+    //Clear all the other effects
+    document.getElementById('icon1').style.border = "none";
+    document.getElementById('icon2').style.border = "none";
+    document.getElementById('icon3').style.border = "none";
+    var image = document.getElementById(name);
+    image.style.border = "5px solid #42A5F5";
+    habitIcon = name;
+}
+
+var habitIcon = null;
+
+function addHabit() {
+    var habitTitle = document.getElementById('title').value;
+    //var habitIcon = document.getElementById(habitIcon).value;
+    var weeklyFreq = {
+        "sun": false,
+        "mon": false,
+        "tues": false,
+        "wed": false,
+        "thurs": false,
+        "fri": false,
+        "sat": false
+    };
+    var dailyFreq = {
+        "one": false,
+        "two": false,
+        "three": false
+    };
+
+    // Set weekly frequency
+    Object.keys(weeklyFreq).forEach(function (key) {
+        weeklyFreq[key] = document.getElementById(key).checked;
+    });
+
+    // Set daily frequency
+    Object.keys(dailyFreq).forEach(function (key) {
+       dailyFreq[key] = document.getElementById(key).checked;
+    });
+
+    var Habit = new Parse.Object.extend("Habit");
+    var habit = new Habit();
+    habit.set("title", habitTitle);
+    habit.set("icon", habitIcon);
+    habit.set("weeklyFreq", weeklyFreq);
+    habit.set("dailyFreq", dailyFreq);
+
+    habit.save(null, {
+            success: function(habit) {
+                alert("Your habit to " + habitTitle + " has been saved!");
+            },
+            error: function(habit, error) {
+                alert("Something went wrong. Your habit was not saved.");
+            }
+        }
+    );
+
+    //window.location = "../src/list.html";
+}
+
+
