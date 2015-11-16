9e04f630368e0b063886244420bd752882e8ce9d
Scott Tang <mashup@Scotts-MacBook-Pro.local>
2015-11-15 22:54:51 -0800

    list page now has correct img icon for each habit

--- js/list.js
+++ js/list.js
@@ -1,4 +1,9 @@
 var username = location.search.substring(1).split("=")[1];
+var habitIconPathHash = {
+    'icon1': '../img/sleep.jpg',
+    'icon2': '../img/salad.jpg',
+    'icon3': '../img/run.jpg'
+};
 
 Parse.initialize("BoImLGZodfLfRb4hR2YkejAdYjtSnLriHunArwnP", "1oCKt23WJYH6jHrjtEqA8wCNRmva9Pfxuj32GLR8");
 
@@ -8,6 +13,10 @@ displayHabits(results);
 
 function addHabitClick() {
     location.href="add.html?username=" + username;
+}
+
+function editHabitClick() {
+    location.href="edit.html?" + username;
 }
 
 // Query habits from database
@@ -20,24 +29,25 @@ function getHabits() {
             displayHabits(results);
         },
         error: function(error) {
-            alert("An error occurred while trying to retreieve your habits.")
+            alert("An error occurred while trying to retrieve your habits.")
         }
     });
 }
 
 function displayHabits(results) {
     var html = '';
+
     for(i = 0; i < results.length; i++) {
         var object = results[i];
         var habitTitle = object.get('title');
-        var habitIcon = object.get('icon');
+        var iconString = object.get('icon');
 
         // HTML
         html    += '<li>'
             +       '<ul class="habit-info\">'
             +           '<li><div class="habit-name">' + habitTitle + '</div></li>'
             +           '<hr class="habitdivider"><br>'
-            +           '<li><img class="habit-icon" src="../img/sleep.jpg" alt="habit icon"></li>'
+            +           '<li><img class="habit-icon" src="' + habitIconPathHash[iconString] + '" alt="habit icon"></li>'
             +       '</ul>'
             +       '<div class="message">'
             +           '<span class="message-total">'
@@ -53,7 +63,7 @@ function displayHabits(results) {
             +           '<button type="button" class="op op-done" onclick="location.href=\'#successModal\'" title="done">'
             +               '<img src="../img/done.svg" alt="Done">'
             +           '</button>'
-            +           '<button type="button" class="op op-edit" onclick="location.href=\'edit.html\'" title="edit habit">'
+            +           '<button type="button" class="op op-edit" onclick="editHabitClick()" title="edit habit">'
             +               '<img src="../img/edit.svg" alt="Edit">'
             +           '</button>'
             +           '<button type="button" class="op op-del" title="delete habit" onclick="location.href=\'#openModal\'">'
